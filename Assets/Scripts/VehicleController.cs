25e4931efb6330c98652c3e9912f22952b412a3a
Calvin Richards <calvinrrichards64@gmail.com>
2026-04-10 00:05:56 -0700

    added obstacles and impulse & turnrate variables

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -5,17 +5,21 @@ public class VehicleController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    private float desired_acceleration;
+    private float desired_acceleration_x;
+    private float desired_acceleration_z;
+    public float impulse;
+    public float turnrate;
     void Start()
     {
-        desired_acceleration = 0;
+        desired_acceleration_x = 0;
+        desired_acceleration_z = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        float dx = (Mouse.current.position.x.value - Screen.width / 2) / 1500; // this is very sensitive (was originally 200)
+        float dx = (Mouse.current.position.x.value - Screen.width / 2) / turnrate; // this is very sensitive (was originally 200)
         // how far the mouse is to the right/left of center of screen, then divided to make it a managable value
     
         if (Mathf.Abs(dx) > 0.01f)
@@ -23,13 +27,14 @@ public class VehicleController : MonoBehaviour
             transform.Rotate(0, dx, 0);
         }
 
-        GetComponent<Rigidbody>().AddRelativeForce(desired_acceleration*2, 0, 0); // force defined in Newtons
+        GetComponent<Rigidbody>().AddRelativeForce(desired_acceleration_x*impulse, 0, desired_acceleration_z*(impulse * 0.66f)); // force defined in Newtons
         // the original 5 was fairly fast
     }   
 
     void OnMove(InputValue action)
     {
         var movement = action.Get<Vector2>();
-        desired_acceleration = movement.y;
+        desired_acceleration_x = movement.y;
+        desired_acceleration_z = -movement.x;
     }
 }
